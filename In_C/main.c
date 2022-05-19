009695f12d0a074642d8dd3a9a5e506c923f3dfb
Zuriel <2001555@sit.singaporetech.edu.sg>
2022-05-19 12:11:44 +0800

    main update

--- In_C/main.c
+++ In_C/main.c
@@ -6,8 +6,32 @@
 
 /* Global Variables */
 
+/* Struct decleration */
+struct Date{
+    int Day;
+    int Month;
+    int Year;
+}; // nested struct > Overhead struct Event
+struct Time{
+    int Hour;
+    int Min;
+    int Sec;
+}; // nested struct > Overhead struct Event
+struct Event{
+    struct Date EventDate;
+    struct Time EventTime;
+}; // Overhead struct Event
+
 int main(){
     /* Initializing Global Variables */
+    struct Event myEvent[100];
+    myEvent[0].EventDate.Day = 4;
+    myEvent[0].EventDate.Month = 6;
+    myEvent[0].EventDate.Year = 22;
+    myEvent[0].EventTime.Hour = 15;
+    myEvent[0].EventTime.Min = 20;
+    myEvent[0].EventTime.Sec = 30;
+    printf("myEvent[0] is Date: %d/%d/%d at %d:%d:%d\n", myEvent[0].EventDate.Day, myEvent[0].EventDate.Month, myEvent[0].EventDate.Year, myEvent[0].EventTime.Hour, myEvent[0].EventTime.Min, myEvent[0].EventTime.Sec);
 
     return 0;
 }
